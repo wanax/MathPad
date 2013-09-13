ad13ffb20872f38ce83fa940193daa14baecd31d
wanax <mxiaochi@gmail.com>
2013-09-13 16:33:00 +0800

    Initial Commit

---
+++ MathMonsters/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MathMonsters
+//
+//  Created by Xcode on 13-9-13.
+//  Copyright (c) 2013年 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
