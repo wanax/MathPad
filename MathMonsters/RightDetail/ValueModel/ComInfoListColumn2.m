695e4548baeb60804002ce4429d156ebe4b8b6d4
wanax <mxiaochi@gmail.com>
2013-11-23 17:17:37 +0800

    估值模型0.6 年份数值label试改进

--- MathMonsters/RightDetail/ValueModel/ComInfoListColumn2.m
+++ MathMonsters/RightDetail/ValueModel/ComInfoListColumn2.m
@@ -69,7 +69,6 @@
         cell=[[[ValueModelCell2 alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ValueModelCell2Identifier] autorelease];
     }
     if(self.comList){
-        id comData=[self.comList objectAtIndex:indexPath.row][@"data"];
         for(UIView *view in cell.contentView.subviews){
             if([view isKindOfClass:NSClassFromString(@"AMProgressView")]){
                 [view removeFromSuperview];
@@ -80,43 +79,44 @@
                 [cell.contentView addSubview:view];
             }
         }
-        [self setCell:cell comData:comData];
+        [self setCell:cell classDic:self.yearValueArr[indexPath.row]];
         cell.contentView.backgroundColor=[Utiles colorWithHexString:@"#452A21"];
     }
     return cell;
 }
 
--(void)setCell:(ValueModelCell2 *)cell comData:(id)comData{
+-(void)setCell:(ValueModelCell2 *)cell classDic:(NSDictionary *)dic{
 
-    [self setCellProcess:cell data:comData[@"Revenue growth rate"] yearLableArr:[NSArray arrayWithObjects:cell.row00,cell.row10,cell.row20,cell.row30, nil] valueLable:[NSArray arrayWithObjects:cell.row01,cell.row11,cell.row21,cell.row31, nil] x:55];
+    [self setCellProcess:cell yearValueDic:dic[@"Revenue growth rate"] yearLableArr:[NSArray arrayWithObjects:cell.row00,cell.row10,cell.row20,cell.row30, nil] valueLable:[NSArray arrayWithObjects:cell.row01,cell.row11,cell.row21,cell.row31, nil] x:55];
     
-    [self setCellProcess:cell data:comData[@"Net income growth rate"] yearLableArr:[NSArray arrayWithObjects:cell.row02,cell.row12,cell.row22,cell.row32, nil] valueLable:[NSArray arrayWithObjects:cell.row03,cell.row13,cell.row23,cell.row33, nil] x:310];
+    [self setCellProcess:cell yearValueDic:dic[@"Net income growth rate"] yearLableArr:[NSArray arrayWithObjects:cell.row02,cell.row12,cell.row22,cell.row32, nil] valueLable:[NSArray arrayWithObjects:cell.row03,cell.row13,cell.row23,cell.row33, nil] x:310];
     
-    [self setCellProcess:cell data:comData[@"Gross profit margin"] yearLableArr:[NSArray arrayWithObjects:cell.row04,cell.row14,cell.row24,cell.row34, nil] valueLable:[NSArray arrayWithObjects:cell.row05,cell.row15,cell.row25,cell.row35, nil] x:570];
+    [self setCellProcess:cell yearValueDic:dic[@"Gross profit margin"] yearLableArr:[NSArray arrayWithObjects:cell.row04,cell.row14,cell.row24,cell.row34, nil] valueLable:[NSArray arrayWithObjects:cell.row05,cell.row15,cell.row25,cell.row35, nil] x:570];
 }
 
--(void)setCellProcess:(ValueModelCell2 *)cell data:(id)data yearLableArr:(NSArray *)labels1 valueLable:(NSArray *)labels2 x:(int)x{
+-(void)setCellProcess:(ValueModelCell2 *)cell yearValueDic:(id)dic yearLableArr:(NSArray *)labels1 valueLable:(NSArray *)labels2 x:(int)x{
+    
+    NSComparator cmptr = ^(id obj1, id obj2){
+        if ([obj1 integerValue] > [obj2 integerValue]) {
+            return (NSComparisonResult)NSOrderedDescending;
+        }
+        
+        if ([obj1 integerValue] < [obj2 integerValue]) {
+            return (NSComparisonResult)NSOrderedAscending;
+        }
+        return (NSComparisonResult)NSOrderedSame;
+    };
     
     NSNumberFormatter *formatter=[[[NSNumberFormatter alloc] init] autorelease];
     [formatter setNumberStyle:NSNumberFormatterPercentStyle];
-    if (![data isKindOfClass:[NSNull class]]) {
-        NSArray *dataArr=nil;
-        if ([data[@"array"] count]>4) {
-            NSMutableArray *temp=[NSMutableArray arrayWithCapacity:4];
-            for(int i=[data[@"array"] count]-1,j=3;j>=0;j--,i--){
-                [temp addObject:data[@"array"][i]];
-            }
-            dataArr=[[temp reverseObjectEnumerator] allObjects];;
-        } else {
-            dataArr=data[@"array"];
-        }
-        
+    if (![dic isKindOfClass:[NSNull class]]) {
         int n=0;
-        for(id obj in dataArr){
+        NSArray *keys=[[dic allKeys] sortedArrayUsingComparator:cmptr];
+        for(id key in keys){
             UILabel *label= labels1[n];
-            [label setText:[NSString stringWithFormat:@"20%@",obj[@"y"]]];
+            [label setText:[NSString stringWithFormat:@"20%@",key]];
             label=labels2[n];
-            [label setText:[formatter stringFromNumber:obj[@"v"]]];
+            [label setText:[formatter stringFromNumber:dic[key]]];
             n++;
         }
     }
@@ -127,23 +127,32 @@
     NSArray *colorArr=[NSArray arrayWithObjects:[Utiles colorWithHexString:@"#fd9e1b"],[Utiles colorWithHexString:@"#f60933"],[Utiles colorWithHexString:@"#03d234"],[Utiles colorWithHexString:@"#0db4b4"], nil];
     
     NSMutableArray *temp=[[[NSMutableArray alloc] init] autorelease];
+    NSMutableArray *tempYearValueArr=[[[NSMutableArray alloc] init] autorelease];
     for(id obj in self.progressArr){
         [temp addObject:obj];
+    }
+    for(id obj in self.yearValueArr){
+        [tempYearValueArr addObject:obj];
     }
     for(id comInfo in self.comList){
         NSMutableArray *tempGrade2=[[[NSMutableArray alloc] init] autorelease];
         
-        [self produceProgressForClass:comInfo[@"data"][@"Revenue growth rate"] tempGrade2:tempGrade2 color:colorArr x:55];
-        [self produceProgressForClass:comInfo[@"data"][@"Net income growth rate"] tempGrade2:tempGrade2 color:colorArr x:310];
-        [self produceProgressForClass:comInfo[@"data"][@"Gross profit margin"] tempGrade2:tempGrade2 color:colorArr x:570];
+        NSMutableDictionary *cellValueDic=[[[NSMutableDictionary alloc] init] autorelease];
+        
+        [self produceProgressForClass:comInfo[@"data"] className:@"Revenue growth rate" tempGrade2:tempGrade2 cellValueDic:cellValueDic color:colorArr x:55];
+        [self produceProgressForClass:comInfo[@"data"] className:@"Net income growth rate" tempGrade2:tempGrade2 cellValueDic:cellValueDic color:colorArr x:310];
+        [self produceProgressForClass:comInfo[@"data"] className:@"Gross profit margin" tempGrade2:tempGrade2 cellValueDic:cellValueDic color:colorArr x:570];
         
         [temp addObject:tempGrade2];
+        [tempYearValueArr addObject:cellValueDic];
     }
     self.progressArr=temp;
+    self.yearValueArr=tempYearValueArr;
 }
 
--(void)produceProgressForClass:(id)classData tempGrade2:(NSMutableArray *)tempArr color:(NSArray *)colorArr x:(int)x{
+-(void)produceProgressForClass:(id)data className:(NSString *)className tempGrade2:(NSMutableArray *)tempArr cellValueDic:(NSMutableDictionary *)cellValueDic color:(NSArray *)colorArr x:(int)x{
 
+    id classData=data[className];
     if (![classData isKindOfClass:[NSNull class]]) {
         NSArray *dataArr=nil;
         if ([classData[@"array"] count]>4) {
@@ -157,10 +166,13 @@
         }
         
         int n=0;
+        NSMutableDictionary *tempGrade2Dic=[[[NSMutableDictionary alloc] init] autorelease];
         for(id obj in dataArr){
             [tempArr addObject:[self makeAProgress:[obj[@"v"] doubleValue] max:1 frame:CGRectMake(x,n*15+2, 165, 10) color:[colorArr objectAtIndex:n]]];
+            [tempGrade2Dic setObject:obj[@"v"] forKey:obj[@"y"]];
             n++;
         }
+        [cellValueDic setObject:tempGrade2Dic forKey:className];
     }
     
 }
